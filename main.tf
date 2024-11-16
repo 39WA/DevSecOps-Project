db569e958f61431643a4c7879b673d8405ce4e4b
wajid <enail>
2024-11-16 15:52:27 +0000

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
+# Setting up S3 bucket 
 provider "aws" {
   region = "eu-west-2" # London region
 }
 resource "aws_s3_bucket" "terraform-projects1" {
-  bucket = "terraform-august-project"
+  bucket = "terraform-projects1"
 }
+
+# Insert Bucket name here!
+terraform {
+  backend "s3" {
+    bucket         = "terraform-projects1"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
+
+# Insert Bucket name here!
